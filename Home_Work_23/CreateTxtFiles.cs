c24e6d4ab45c5ea2b40424b926389e5a1887b82f
aziz <aziz@DESKTOP-B2RAR63>
2023-06-25 01:51:55 +0500

    Добавьте файлы проекта.

---
+++ Home_Work_23/CreateTxtFiles.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work_23
+{
+    public static class CreateTxtFilesAndWrite
+    {
+        public static void CreateFiles_10()
+        {
+            // Yangi fayl ochish va yozish
+            for (int i = 0; i < 10; i++)
+            {
+            string cn = Convert.ToString(i);
+            string path = $@"C:\\Users\\aziza\\source\\repos\\Home_Work_23\\Home_Work_23\\TxtFiles\\txt{cn}.txt";
+            var f1 = new FileInfo(path);
+                using (StreamWriter sw = f1.CreateText())
+                {
+                    sw.WriteLine(i);
+                }
+            }
+        }
+
+
+        public static void ReadAllFiles()
+        {
+            // fayl ochish va Uqish
+            for (int i = 0; i < 10; i++)
+            {
+            string cn = Convert.ToString(i);
+            string path = $@"C:\\Users\\aziza\\source\\repos\\Home_Work_23\\Home_Work_23\\TxtFiles\\txt{i}.txt";
+            var f1 = new FileInfo(path);
+            
+                using (StreamReader sr = f1.OpenText())
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+
+        }
+                
+    }
+}
---
+++ Home_Work_23/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using Home_Work_23;
+
+CreateTxtFilesAndWrite.CreateFiles_10();
+CreateTxtFilesAndWrite.ReadAllFiles();
+
